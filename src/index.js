0648fe751fc49cf21d3dee6c9c06a8f3a619b75c
your GitHub username <your GitHub email>
2026-05-15 20:32:59 -0500

    Initial Morphiq commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Morphiq from './Morphiq';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Morphiq />);
