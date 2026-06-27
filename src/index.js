b8217b09c0081bb7b6fa56020fc543892ce1d6d5
Luxurydadbot <bcarbonell@sbcglobal.net>
2026-06-27 11:23:01 -0500

    Revert: restore index.js to import from Morphiq (not AppRouter)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Morphiq from './AppRouter';
+import Morphiq from './Morphiq';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Morphiq />);
